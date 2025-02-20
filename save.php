cf47cc66eda7eb386622aecb96e9ab0ceee17403
oduvanchek <falloutchel@gmail.com>
2025-02-20 14:03:43 +0700

    init

--- save.php
+++ save.php
@@ -1,5 +1,5 @@
 <?php
-$data = "Имя: {$_POST['name']}\nEmail: {$_POST['email']}\nСообщение: {$_POST['email']}\n--------------\n";
+$data = "Имя: {$_POST['name']}\nEmail: {$_POST['email']}\nСообщение: {$_POST['message']}\n--------------\n";
 
 file_put_contents(__DIR__ . "feedback.txt", $data, FILE_APPEND);
 
