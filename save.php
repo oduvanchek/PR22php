36c3e16fa88424fb88f804fee0265250af214ffe
oduvanchek <falloutchel@gmail.com>
2025-02-20 13:55:13 +0700

    init

---
+++ save.php
@@ -0,0 +1,6 @@
+<?php
+$data = "Имя: {$_POST['name']}\nEmail: {$_POST['email']}\nСообщение: {$_POST['email']}\n--------------\n";
+
+file_put_contents(__DIR__ . "fideback.txt", $data, FILE_APPEND);
+
+echo "Спасибо за отзыв!";
